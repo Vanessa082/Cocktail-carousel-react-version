ed9f0c98c0d567c31d4d11bef6948e5cdceace6b
vanessa <wahvanessa22@gmail.com>
2024-04-17 10:18:24 +0100

    created  a component display a carousel

---
+++ src/assets/Components/Carusel/Carousel.jsx
@@ -0,0 +1,20 @@
+function carousel() {
+  const[]
+  return (
+    <main>
+      <h1>Cocktail reciept</h1>
+
+      <div class="carousel-container">
+        <div class="selected-drink active">
+          <img src="#" alt="drink-image" class="drink-image" />
+          <h2 class="drink-name"></h2>
+          <div class="recipe-dropdown"></div>
+          <button class="recipe-button">View Recipe</button>
+        </div>
+
+        <button class="prev">&#10094;</button>
+        <button class="next">&#10095;</button>
+      </div>
+    </main>
+  );
+}
